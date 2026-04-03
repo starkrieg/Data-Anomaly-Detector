00e9670dccf07bbee8b6c2567dc7f9bcf60690e2
starkrieg <93294578+starkrieg@users.noreply.github.com>
2026-04-03 15:42:58 -0300

    feat: Apply Welford's Variance algorithm to improve variance calculation

--- Data-Anomaly-Detector/src/test/java/com/application/detector/ZScoreAnomalyDetectorTest.java
+++ Data-Anomaly-Detector/src/test/java/com/application/detector/ZScoreAnomalyDetectorTest.java
@@ -10,8 +10,10 @@ public class ZScoreAnomalyDetectorTest {
         try {
             ZScoreAnomalyDetector zScoreAnomalyDetector = new ZScoreAnomalyDetector(0, 3.5);
             Assertions.fail("Expected exception when creating object with invalid args");
-        } catch (Exception ex) {
+        } catch (IllegalArgumentException ex) {
             Assertions.assertTrue(true);
+        } catch (Exception ex) {
+            Assertions.fail("Got wrong type of exception, expected IllegalArgumentException");
         }
     }
 
@@ -20,8 +22,10 @@ public class ZScoreAnomalyDetectorTest {
         try {
             ZScoreAnomalyDetector zScoreAnomalyDetector = new ZScoreAnomalyDetector(50, -1.5);
             Assertions.fail("Expected exception when creating object with invalid args");
-        } catch (Exception ex) {
+        } catch (IllegalArgumentException ex) {
             Assertions.assertTrue(true);
+        } catch (Exception ex) {
+            Assertions.fail("Got wrong type of exception, expected IllegalArgumentException");
         }
     }
 
@@ -37,75 +41,77 @@ public class ZScoreAnomalyDetectorTest {
 
     @Test
     public void testZScoreCalculation_Success() {
-        ZScoreAnomalyDetector zScoreAnomalyDetector = new ZScoreAnomalyDetector(50, 3.5);
+        int datasetSize = 50;
+        double zetaScoreThreshold = 3.5;
 
-        // TODO - Change this test to properly validate the internals of the calculation
-        // actual Z-Score and the Normal vs Anomaly status logs
+        // Instance of the class that will be tested
+        ZScoreAnomalyDetector zScoreAnomalyDetector = new ZScoreAnomalyDetector(datasetSize, zetaScoreThreshold);
 
-        // For now, just perform a series of validations and check that no errors occur
-        zScoreAnomalyDetector.validateDataPoint(2.50);
-        zScoreAnomalyDetector.validateDataPoint(2.50);
-        zScoreAnomalyDetector.validateDataPoint(2.50);
-        zScoreAnomalyDetector.validateDataPoint(2.50);
-        zScoreAnomalyDetector.validateDataPoint(2.50);
-        zScoreAnomalyDetector.validateDataPoint(2.50);
-        zScoreAnomalyDetector.validateDataPoint(2.50);
-        zScoreAnomalyDetector.validateDataPoint(2.50);
-        zScoreAnomalyDetector.validateDataPoint(2.50);
-        zScoreAnomalyDetector.validateDataPoint(3.50);
-        zScoreAnomalyDetector.validateDataPoint(3.50);
-        zScoreAnomalyDetector.validateDataPoint(3.50);
-        zScoreAnomalyDetector.validateDataPoint(1.50);
-        zScoreAnomalyDetector.validateDataPoint(1.50);
-        zScoreAnomalyDetector.validateDataPoint(1.50);
-        zScoreAnomalyDetector.validateDataPoint(2.50);
-        zScoreAnomalyDetector.validateDataPoint(2.50);
-        zScoreAnomalyDetector.validateDataPoint(3.50);
-        zScoreAnomalyDetector.validateDataPoint(3.50);
-        zScoreAnomalyDetector.validateDataPoint(3.50);
-        zScoreAnomalyDetector.validateDataPoint(4.50);
-        zScoreAnomalyDetector.validateDataPoint(5.50);
-        zScoreAnomalyDetector.validateDataPoint(3.50);
-        zScoreAnomalyDetector.validateDataPoint(3.50);
-        zScoreAnomalyDetector.validateDataPoint(3.50);
-        zScoreAnomalyDetector.validateDataPoint(3.50);
-        zScoreAnomalyDetector.validateDataPoint(2.50);
-        zScoreAnomalyDetector.validateDataPoint(2.50);
-        zScoreAnomalyDetector.validateDataPoint(3.50);
-        zScoreAnomalyDetector.validateDataPoint(3.50);
-        zScoreAnomalyDetector.validateDataPoint(4.50);
-        zScoreAnomalyDetector.validateDataPoint(4.50);
-        zScoreAnomalyDetector.validateDataPoint(3.50);
-        zScoreAnomalyDetector.validateDataPoint(3.50);
-        zScoreAnomalyDetector.validateDataPoint(2.50);
-        zScoreAnomalyDetector.validateDataPoint(-0.83); // Status: ANOMALY DETECTED
-        zScoreAnomalyDetector.validateDataPoint(1.37);
-        zScoreAnomalyDetector.validateDataPoint(1.28);
-        zScoreAnomalyDetector.validateDataPoint(0.47);
-        zScoreAnomalyDetector.validateDataPoint(-0.28);
-        zScoreAnomalyDetector.validateDataPoint(-0.42);
-        zScoreAnomalyDetector.validateDataPoint(-1.36);
-        zScoreAnomalyDetector.validateDataPoint(31.88); // Status: ANOMALY DETECTED
-        zScoreAnomalyDetector.validateDataPoint(-0.84);
-        zScoreAnomalyDetector.validateDataPoint(1.17);
-        zScoreAnomalyDetector.validateDataPoint(-0.42);
-        zScoreAnomalyDetector.validateDataPoint(0.84);
-        zScoreAnomalyDetector.validateDataPoint(-0.30);
-        zScoreAnomalyDetector.validateDataPoint(1.15);
-        zScoreAnomalyDetector.validateDataPoint(-0.29);
-        zScoreAnomalyDetector.validateDataPoint(0.35);
-        zScoreAnomalyDetector.validateDataPoint(-0.27);
-        zScoreAnomalyDetector.validateDataPoint(-1.16);
-        zScoreAnomalyDetector.validateDataPoint(-1.60);
-        zScoreAnomalyDetector.validateDataPoint(-0.25);
-        zScoreAnomalyDetector.validateDataPoint(0.22);
-        zScoreAnomalyDetector.validateDataPoint(-0.13);
-        zScoreAnomalyDetector.validateDataPoint(0.04);
-        zScoreAnomalyDetector.validateDataPoint(-1.26);
-        zScoreAnomalyDetector.validateDataPoint(-1.54);
+        // An instance of a Naive implementation of the tested class
+        // Used to validate the outputs of the tested class
+        ZScoreAnomalyDetectorNaive zScoreNaiveTest = new ZScoreAnomalyDetectorNaive(datasetSize);
 
-        // If no errors until here, Pass
-        Assertions.assertTrue(true);
+        // A sequence of data points that is BIGGER than the dataset size defined previously
+        // This way the removal of old values can also be tested
+        double[] values = new double[]{
+                2.50                ,2.50                ,2.50                ,2.50                ,2.50
+                ,2.50                ,2.50                ,2.50                ,2.50                ,3.50
+                ,3.50                ,3.50                ,1.50                ,1.50                ,1.50
+                ,2.50                ,2.50                ,3.50                ,3.50                ,3.50
+                ,4.50                ,5.50                ,3.50                ,3.50                ,3.50
+                ,3.50                ,2.50                ,2.50                ,3.50                ,3.50
+                ,4.50                ,4.50                ,3.50                ,3.50                ,2.50
+                ,-0.83 // Status: ANOMALY DETECTED
+                ,1.37                ,1.28                ,0.47                ,-0.28                ,-0.42
+                ,-1.36                ,31.88 // Status: ANOMALY DETECTED
+                ,-0.84                ,1.17                ,-0.42                ,0.84                ,-0.30
+                ,1.15                ,-0.29                ,0.35                ,-0.27                ,-1.16
+                ,-1.60                ,-0.25                ,0.22                ,-0.13                ,0.04
+                ,-1.26                ,-1.54
+        };
+
+        // Confirm that we are using a big enough array of data points
+        Assertions.assertTrue(values.length > datasetSize, "Array of data points is not big enough");
+
+        // TODO - Add test for Normal vs Anomaly status logs, probably using verify or stub
+
+        for (double dataPoint : values) {
+            // Add datapoint to the baseline algorithm
+            zScoreNaiveTest.validateDataPoint(dataPoint);
+
+            // Add datapoint to the algorithm to be validated
+            zScoreAnomalyDetector.validateDataPoint(dataPoint);
+
+            // Perform comparison of the internal calculation steps below
+
+            // Because of the algorithms used, there can be minimal differences on the trailing floating points between
+            // the Means and Variations
+            // This is inherent to floating point numbers in calculations, that doesn't mean the calculation is wrong
+            // Using simple double == double or even String.format("%.2f") will eventually fail
+            //
+            // Validation of these values will be done by checking if they are "nearly" identical
+            // Meaning we will use an error margin or tolerance
+            //
+            // Tolerance = 1e-12 // 0,000.000.000.000.1 (12 zeros)
+            // A very low tolerance since this is for statistical analysis
+            double tolerance = 1e-12;
+
+            // Assert that the Mean is the same
+            double differenceMean = Math.abs(zScoreNaiveTest.getMean() - zScoreAnomalyDetector.getMean());
+            Assertions.assertTrue(differenceMean < tolerance,
+                    "Calculated mean is over the tolerance of " + tolerance);
+
+            // Assert that the Mean is the same
+            double differenceVariation = Math.abs(zScoreNaiveTest.getStandardDeviation() - zScoreAnomalyDetector.getStandardDeviation());
+            Assertions.assertTrue(differenceVariation < tolerance,
+                    "Calculated Standard Deviation is over the tolerance of " + tolerance);
+
+            // Assert that the Z-Score is the same
+            double differenceZScore = Math.abs(zScoreNaiveTest.getLastZScore() - zScoreAnomalyDetector.getLastZScore());
+            Assertions.assertTrue(differenceZScore < tolerance,
+                    "Calculated Z-Score is over the tolerance of " + tolerance);
+        }
+
     }
 
 }
